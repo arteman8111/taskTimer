7ab6b5cc7025aa7d523ecdaaedaa37fbc2b9d951
ARTEM DEMCHENKO <arteman8112@gmail.com>
2024-08-22 21:57:27 +0300

    #26 Страница активностей

--- src/components/TheActivities.vue
+++ src/components/TheActivities.vue
@@ -1,5 +1,45 @@
 <template>
-    <h1>
-        Activities
-    </h1>
+    <div>
+        <ul class="divide-y">
+            <li v-for="activity in activities" :key="activity" class="flex flex-col gap-2 p-4">
+                <div class="flex items-center gap-2">
+                    <BaseButton>
+                        <TrashIcon class="h-8"></TrashIcon>
+                    </BaseButton>
+                    <span class="truncate text-xl">{{activity}}</span>
+                </div>
+                <div>
+                    <BaseSelect class="font-mono" placeholder="h:mm" :options="periodSelectOptions"/>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script setup>
+import { TrashIcon } from '@heroicons/vue/24/solid';
+import BaseButton from './BaseButton.vue';
+import BaseSelect from './BaseSelect.vue';
+
+const activities = ['Coding', 'Reading', 'Training'];
+
+const periodSelectOptions = [
+    {
+        value: 15,
+        label: '0:15'
+    },
+    {
+        value: 30,
+        label: '0:30'
+    },
+    {
+        value: 45,
+        label: '0:45'
+    },
+]
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
