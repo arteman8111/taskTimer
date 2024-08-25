e366ab2d6d9bcc0bc42c66d45bde43c74cb10e6b
ARTEM DEMCHENKO <arteman8112@gmail.com>
2024-08-25 21:04:28 +0300

    #35 Компонент формы создания активности

--- src/components/TheActivities.vue
+++ src/components/TheActivities.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { PlusIcon } from '@heroicons/vue/24/solid'
 import ActivityItem from './ActivityItem.vue'
-import BaseButton from './BaseButton.vue'
+import TheActivityForm from './TheActivityForm.vue'
 import { isActivityValid, validateActivities } from '@/validators'
 
 defineProps({
@@ -16,7 +15,6 @@ const emit = defineEmits({
   createActivity: isActivityValid
 })
 
-let newActivity = 'new'
 </script>
 
 <template>
@@ -28,16 +26,5 @@ let newActivity = 'new'
       @delete="emit('deleteActivity', activity)"
     />
   </ul>
-  <form @submit.prevent="emit('createActivity', newActivity)" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
-    <input
-      type="text"
-      :value="newActivity"
-      @input="newActivity = $event.target.value"
-      class="w-full rounded border px-4 text-xl"
-      placeholder="Activity name"
-    />
-    <BaseButton>
-      <PlusIcon class="h-8" />
-    </BaseButton>
-  </form>
+  <TheActivityForm @submit="emit('createActivity', $event)"/>
 </template>
